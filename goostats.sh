fff5a1fbffed4f6a2267e44bfc0489ea678aeebd
Ashlyn <ashlyn.poole@postgrad.curtin.edu.au>
2025-10-07 14:47:48 +0800

    Compute the range of values

--- goostats.sh
+++ goostats.sh
@@ -6,3 +6,4 @@ echo "Welcome to Nelle's stat script"
 # Compute the min/max/range of values in a file
 min=$( cat ${fname} | sort | head -1)
 min=$( cat ${fname} | sort | tail -1)
+range=$( echo "${max}-${min}" | bc -l)
